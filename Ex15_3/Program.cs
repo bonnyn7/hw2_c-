b6954d65656319a116b3a585d21f2e909f42b951
Anastasia <bonnyn7@yandex.ru>
2022-06-28 15:52:49 +0300

    Added ex3

---
+++ Ex15_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число, соответствующее дню недели: ");
+        int day = Convert.ToInt32(Console.ReadLine());
+        void check (int day)
+        {
+        if (day == 6 || day == 7) 
+        {
+            Console.WriteLine("Это выходной день");
+        }
+        else if (day < 1 || day> 7)
+        {
+            Console.WriteLine("Такого дня нет");
+        }
+        else 
+        {
+            Console.WriteLine("Это не выходной день");
+        }
+        }
+        check(day);
+    }
+}
