e84c5616ded282dde7ac7d98644aa132a6f060d6
Anastasia <bonnyn7@yandex.ru>
2022-06-28 13:15:44 +0300

    Added 1 ex

---
+++ Ex10_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        string str = Convert.ToString(num);
+        Console.WriteLine("Вторая цифра введенного числа: " + str[1]);
+    }
+}
